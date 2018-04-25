d14e02aa8753ab1aa18cfe7036e2184d4bacceac
Jamal093 <38686772+Jamal093@users.noreply.github.com>
2018-04-25 11:13:24 +0300

    Version1
    List the text files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Jamal\Desktop\Directory";
+            string files = Path.GetFileName(path);
+
+            foreach (string s in Directory.GetFiles(path, "*.txt").Select(Path.GetFileName))
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
