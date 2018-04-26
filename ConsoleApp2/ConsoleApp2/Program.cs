ee4b849ad4ba51a0e63cb5068107d3709bb059a6
omerkngr <omerkngr@gmail.com>
2018-04-26 16:26:53 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            string path = @"C:\Users\Jamal\Desktop\Directory";
+            //string files = Path.GetFileNameWithoutExtension(path);
+
+            Console.WriteLine("Enter (.txt) file name listed below:");
+
+            foreach (string s in Directory.GetFiles(path, "*.txt").Select(Path.GetFileNameWithoutExtension))
+            {
+
+                Console.Write(s);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.Write(":");
+            input = Console.ReadLine();
+            //Console.WriteLine(input);
+            string text = File.ReadAllText(@"C:\Users\Jamal\Desktop\Directory\" + input + ".txt");
+
+
+            //Console.WriteLine(text);
+
+            int count = text.Split(' ').Length;
+            Console.WriteLine("There are " + count + " word(s) in " + input + ".txt file.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
